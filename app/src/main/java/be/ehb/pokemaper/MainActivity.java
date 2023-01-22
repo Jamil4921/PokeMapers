115803fb2dde8a86f8849ff6891570b32bfa10f0
furqan <furqan.jamil.chaud-ry@student.ehb.be>
2023-01-22 15:19:55 +0100

    added logout button

--- app/src/main/java/be/ehb/pokemaper/MainActivity.java
+++ app/src/main/java/be/ehb/pokemaper/MainActivity.java
@@ -4,14 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity{
 
-    private Button btnSignOut;
+
     private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,19 +22,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mAuth = FirebaseAuth.getInstance();
 
-        btnSignOut = findViewById(R.id.btnSignOut);
-        btnSignOut.setOnClickListener(this);
-
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch(v.getId()){
-            case R.id.btnSignOut:
+        Button btnSignOut = findViewById(R.id.btnSignOut);
+        btnSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 signOut();
-                break;
-        }
+            }
+        });
+
     }
+
 
     private void signOut(){
         FirebaseAuth.getInstance().signOut();
